fe909af52da9b57fab3b5603e0cff409297e14b5
blacktop <blacktop@users.noreply.github.com>
2016-05-29 12:38:51 -0600

    Revert "I don't need to have my own copy of drivers"
    This reverts commit d8fa9e7533249affb2ab5b1bcf509c0ea5497aa3.

--- clients/VBoxManage/cmd/list.go
+++ clients/VBoxManage/cmd/list.go
@@ -15,7 +15,8 @@
 package cmd
 
 import (
-	"github.com/docker/machine/drivers/virtualbox"
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -23,11 +24,15 @@ import (
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "A brief description of your command",
-	Long:  `A longer description that spans multiple lines`,
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		vbm := virtualbox.NewVBoxManager()
-		vbm.vbm()
-		// vbm ("list", "vms")
+		// TODO: Work your own magic here
+		fmt.Println("list called")
 	},
 }
 
